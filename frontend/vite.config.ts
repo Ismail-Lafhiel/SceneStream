3a9caf491ffbc8ca65531b871ae1c27912e84bca
Ismail-Lafhiel <odashimidorida@gmail.com>
2025-03-21 14:18:42 +0000

    SCEN-25 Created dockerfile for teh frontend and update the docker compose to handle it

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,16 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: true,
     port: 3000,
+    strictPort: true,
+    watch: {
+      usePolling: true,
+    },
   },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
-  envDir: '.',
-})
+  build: {
+    outDir: "dist",
+    sourcemap: true,
+  },
+  envDir: ".",
+});
